2ac413fa1d0bc628068d641f920eda2bf4f1ee26
Risky Armansyah <rarmnsyah787@gmail.com>
2023-02-10 19:37:17 +0700

    update scrapping twitter
    menyelesaikan program scraping twit menggunakan twitter di file scrapping-twitter.py, sebagai contoh kita sudah dapat 10000 data dari search "islam'

--- MEGA-PROJECT/scrapping-twitter.py
+++ MEGA-PROJECT/scrapping-twitter.py
@@ -5,19 +5,19 @@ import pandas as pd
 
 # dirpath = os.path.dirname(os.path.realpath(__file__)) 
 
-def twint_search(search, name_file, lang = 'id', limit = 10):
+def twint_search(search, name_file, lang = 'id', limit = 10, store = False):
     config = twint.Config()
     config.Search = search
     config.Lang = lang
     config.Limit = limit
-    config.Store_csv = True
-    config.Output(name_file)
+    config.Store_json = store
+    config.Output = name_file
 
     twint.run.Search(config)
 
 
     # return config.output()
 
-test_search = twint_search('islam', 'store_twit_islam.csv')
+test_search = twint_search('islam', 'store_twit_islam.json', store = True, limit = 10000)
 # print(dirpath)
 # print(test_search)
