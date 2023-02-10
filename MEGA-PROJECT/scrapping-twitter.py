961d5243297b90a1cb347ac5d36585d354249000
Risky Armansyah <rarmnsyah787@gmail.com>
2023-02-10 21:18:36 +0700

    fix errors on scrapping twitter function
    berhasil menyelesaikan permasalahan pada fungsi scraping twitter. Selain itu, juga berhasil mengambil sejumlah 2 dataset dengan kode pencarian islam sebanyak 10000 baris data, dan kode muslim sebanyak 20000 baris data

--- MEGA-PROJECT/scrapping-twitter.py
+++ MEGA-PROJECT/scrapping-twitter.py
@@ -5,12 +5,12 @@ import pandas as pd
 
 # dirpath = os.path.dirname(os.path.realpath(__file__)) 
 
-def twint_search(search, name_file, lang = 'id', limit = 10, store = False):
+def twint_search(search, name_file, lang = 'id', limit = 10, store = True):
     config = twint.Config()
     config.Search = search
     config.Lang = lang
     config.Limit = limit
-    config.Store_json = store
+    config.Store_csv = store
     config.Output = name_file
 
     twint.run.Search(config)
@@ -18,6 +18,7 @@ def twint_search(search, name_file, lang = 'id', limit = 10, store = False):
 
     # return config.output()
 
-test_search = twint_search('islam', 'store_twit_islam.json', store = True, limit = 10000)
+# test_search = twint_search('islam', 'store_twit_islam.json', store = True, limit = 10000)
+twint_search('muslim', 'store_twit_muslim.csv', limit = 20000)
 # print(dirpath)
 # print(test_search)
