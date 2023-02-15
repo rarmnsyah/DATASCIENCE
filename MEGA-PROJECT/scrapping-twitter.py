2399931b3404c092ecc92b3ce65ba799fc1036af
Risky Armansyah <rarmnsyah787@gmail.com>
2023-02-15 09:49:11 +0700

    daily updates
    keterangan di word

--- MEGA-PROJECT/scrapping-twitter.py
+++ MEGA-PROJECT/scrapping-twitter.py
@@ -1,11 +1,12 @@
-import os 
+import os
 import twint
 import numpy as np
 import pandas as pd
 
-# dirpath = os.path.dirname(os.path.realpath(__file__)) 
+# dirpath = os.path.dirname(os.path.realpath(__file__))
 
-def twint_search(search, name_file, lang = 'en', limit = 10, store = True):
+
+def twint_search(search, name_file, lang='en', limit=10, store=True):
     config = twint.Config()
     config.Search = search
     config.Lang = lang
@@ -15,10 +16,12 @@ def twint_search(search, name_file, lang = 'en', limit = 10, store = True):
 
     twint.run.Search(config)
 
-
     # return config.output()
 
+
+keyword = 'jokowi'
 # test_search = twint_search('islam', 'store_twit_islam.json', store = True, limit = 10000)
-twint_search('islam', 'store_twit_islam.csv', limit = 20000)
+twint_search(
+    keyword, 'databases/store_twit_{}.csv'.format(keyword), limit=20000)
 # print(dirpath)
 # print(test_search)
